04a8aeda6500fa0e2c7a4a8a12129a015ae12d3d
priya <bhargavi1791@gmail.com>
2020-03-02 19:36:57 -0600

    Adding user-agent header to spoonacular calls

--- src/main/java/com/example/calorie/counter/services/SearchService.java
+++ src/main/java/com/example/calorie/counter/services/SearchService.java
@@ -4,6 +4,10 @@ import com.example.calorie.counter.model.ItemDetailsResult;
 import com.example.calorie.counter.model.ItemSearchResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,6 +20,7 @@ public class SearchService {
     @Autowired
     private Environment environment;
 
+    RestTemplate restTemplate = new RestTemplate();
 
     private static String url1 = "https://api.spoonacular.com/recipes/search?apiKey={apiKey}&query={query}";
 
@@ -23,31 +28,47 @@ public class SearchService {
 
     public ItemSearchResult search(String searchTerm){
 
-        RestTemplate restTemplate = new RestTemplate();
 
         Map<String, String> queryParams = new HashMap<>();
         queryParams.put("apiKey", environment.getProperty("api.key"));
         queryParams.put("query", searchTerm);
 
-        ItemSearchResult result = restTemplate.getForObject(url1, ItemSearchResult.class, queryParams);
+        HttpEntity<String> entity = getStringHttpEntity();
+
+        ResponseEntity<ItemSearchResult> responseObj = restTemplate.exchange(url1,
+                HttpMethod.GET, entity, ItemSearchResult.class, queryParams);
+        ItemSearchResult result = responseObj.getBody();
+
+//        ItemSearchResult result = restTemplate.getForObject(url1, ItemSearchResult.class, queryParams);
 
         return result;
-
-
     }
 
     public ItemDetailsResult search2(String itemId){
-
-        RestTemplate restTemplate = new RestTemplate();
 
         Map<String, String> queryParams = new HashMap<>();
         queryParams.put("id", itemId);
         queryParams.put("apiKey", environment.getProperty("api.key"));
 
-        ItemDetailsResult result = restTemplate.getForObject(url2, ItemDetailsResult.class, queryParams);
+
+        HttpEntity<String> entity = getStringHttpEntity();
+
+        ResponseEntity<ItemDetailsResult> responseObj = restTemplate.exchange(url2,
+                HttpMethod.GET, entity, ItemDetailsResult.class, queryParams);
+        ItemDetailsResult result = responseObj.getBody();
+
+//        ItemDetailsResult result = restTemplate.getForObject(url2, ItemDetailsResult.class, queryParams);
 
         return result;
 
     }
+
+    private static HttpEntity<String> getStringHttpEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36");
+        return new HttpEntity<>("parameters", headers);
+    }
+
+
 }
 
